33862aa30a90dadf543b91d7ee8c9f029aa55a96
Lakshay Manchanda <lakshaymanchanda06@gmail.com>
2025-05-10 18:26:56 -0700

    added titles

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,85 @@
 import { Canvas } from "@react-three/fiber";
 import Scene from "./Components/Scene";
-import { Loader, PerspectiveCamera } from "@react-three/drei";
-import { Suspense } from "react";
+import { Loader, OrbitControls, PerspectiveCamera } from "@react-three/drei";
+import { Suspense, useRef } from "react";
+import * as THREE from "three";
+import { useGSAP } from "@gsap/react";
+import { gsap } from "gsap";
+
+const convertToSpan = (str: string, underline: boolean = false) => {
+  return str.split("").map((char, i) => (
+    <span className={`inline-block ${underline && "underline"}`} key={i}>
+      {char}
+    </span>
+  ));
+};
 
 function App() {
-  return (
-    <div className="w-screen h-screen ">
-      <Suspense fallback={<Loader />}>
-        <Canvas className="w-screen h-screen" shadows>
-          {/* <HeroText /> */}
-          <color attach="background" args={["#0d0d0d"]} />
-          <PerspectiveCamera
-            makeDefault
-            position={[-0.8, 0.7, -1.8]}
-            fov={50}
-            rotation={[0.2, 3.5, 0.08]}
-          />
+  const cameraRef = useRef<THREE.PerspectiveCamera>(null);
+  const koenigseggRef = useRef<HTMLHeadingElement>(null);
+  const gemeraRef = useRef<HTMLHeadingElement>(null);
 
-          <Scene />
-        </Canvas>
-      </Suspense>
-    </div>
+  useGSAP(() => {
+    if (!koenigseggRef.current) return;
+    if (!gemeraRef.current) return;
+    gsap.from(koenigseggRef.current.children, {
+      opacity: 0,
+      duration: 0.4,
+      scale: 1.1,
+      delay: 2,
+      ease: "power4.in",
+      stagger: 0.05,
+    });
+    gsap.from(gemeraRef.current.children, {
+      opacity: 0,
+      duration: 0.6,
+      scale: 1.1,
+      delay: 2,
+      ease: "power4.in",
+      stagger: -0.05,
+    });
+  }, []);
+
+  return (
+    <Suspense fallback={<Loader />}>
+      <div className="w-screen h-screen ">
+        <section>
+          <h1
+            className="text-white text-8xl font-['Koenigsegg'] font-bold italic fixed left-[10%] top-[3%]"
+            ref={koenigseggRef}
+          >
+            {convertToSpan("Koenigsegg", true)}
+          </h1>
+          <h1
+            className="text-white text-[7rem] font-['Gemera']  fixed bottom-[0%] right-[10%] "
+            ref={gemeraRef}
+          >
+            {convertToSpan("Gemera")}
+          </h1>
+        </section>
+        <section className="fixed -z-10 h-screen w-screen inset-0">
+          <Canvas className="w-screen h-screen" shadows>
+            {/* <HeroText /> */}
+            <color attach="background" args={["#0d0d0d"]} />
+            <PerspectiveCamera
+              ref={cameraRef}
+              makeDefault
+              position={[-0.8, 0.78, -1.8]}
+              fov={50}
+              rotation={[0.3, 3.6, 0.13]}
+            />
+
+            {/* <OrbitControls
+            enableZoom={true}
+            enablePan={true}
+            enableRotate={true}
+            maxPolarAngle={Math.PI / 2}
+            /> */}
+            <Scene position={[0, 0, 0]} cameraRef={cameraRef} />
+          </Canvas>
+        </section>
+      </div>
+    </Suspense>
   );
 }
 
--- src/Components/Scene.tsx
+++ src/Components/Scene.tsx
@@ -2,31 +2,56 @@ import { forwardRef, useRef } from "react";
 import Koenigsegg from "./Koenigsegg";
 import { useFrame } from "@react-three/fiber";
 import * as THREE from "three";
+import { Circle } from "@react-three/drei";
+import { useGSAP } from "@gsap/react";
+import { gsap } from "gsap";
 
-const Scene = forwardRef((props, ref) => {
-  const directionLightRef = useRef<THREE.DirectionalLight>(null);
+type SceneProps = {
+  cameraRef: React.RefObject<THREE.PerspectiveCamera | null>;
+  position?: [number, number, number];
+} & React.ComponentPropsWithRef<"group">;
 
-  useFrame(() => {
-    if (directionLightRef.current) {
-      if (directionLightRef.current.intensity <= 1)
-        directionLightRef.current.intensity += 0.0075;
-    }
-  });
-  return (
-    <group {...props} ref={ref}>
-      <directionalLight
-        ref={directionLightRef}
-        position={[5, 5, 5]}
-        intensity={0}
-      />
-      <Koenigsegg />
-      <mesh rotation={[-Math.PI / 2, 0, 0]} receiveShadow position={[0, 0, 0]}>
-        <planeGeometry args={[10, 10]} />
-        {/* <axesHelper /> */}
-        <meshStandardMaterial color={"#141414"} />
-      </mesh>
-    </group>
-  );
-});
+const Scene = forwardRef<THREE.Group, SceneProps>(
+  ({ cameraRef, ...props }, ref) => {
+    const directionLightRef = useRef<THREE.DirectionalLight>(null);
+
+    useGSAP(() => {
+      if (!cameraRef.current) return;
+      gsap.from(cameraRef.current!.position, {
+        x: 10, // starting x position
+        y: 5, // starting y position
+        z: 10, // starting z position
+        // duration: 2.5,
+        duration: 7,
+        ease: "power2.out",
+      });
+    }, []);
+
+    useFrame(() => {
+      if (directionLightRef.current) {
+        if (directionLightRef.current.intensity <= 1)
+          directionLightRef.current.intensity += 0.0075;
+      }
+    });
+    return (
+      <group {...props} ref={ref}>
+        <directionalLight
+          ref={directionLightRef}
+          position={[5, 5, 5]}
+          intensity={0}
+        />
+        <Circle
+          rotation={[-Math.PI / 2, 0, 0]}
+          receiveShadow
+          args={[1.4]}
+          position={[0, 0, 0]}
+        >
+          <meshStandardMaterial color={"#141414"} roughness={0.9} />
+        </Circle>
+        <Koenigsegg />
+      </group>
+    );
+  }
+);
 
 export default Scene;

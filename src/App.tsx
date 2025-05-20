5ae389d602015d68d14209383ae2e8375e4fd034
Lakshay Manchanda <lakshaymanchanda06@gmail.com>
2025-05-19 20:10:58 -0700

    scroll trigger with dummy section working

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { Canvas } from "@react-three/fiber";
 import Scene from "./Components/Scene";
 import { Loader, OrbitControls, PerspectiveCamera } from "@react-three/drei";
-import { Suspense, useRef } from "react";
+import { Suspense, useRef, useEffect, useState } from "react";
 import * as THREE from "three";
 import { useGSAP } from "@gsap/react";
 import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useControls } from "leva";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const convertToSpan = (str: string, underline: boolean = false) => {
   return str.split("").map((char, i) => (
@@ -14,14 +18,80 @@ const convertToSpan = (str: string, underline: boolean = false) => {
   ));
 };
 
+const DEFAULT_CAMERA_POSITION = [-0.8, 0.78, -1.8];
+const DEFAULT_CAMERA_ROTATION = [0.3, 3.6, 0.13];
+const ENGINE_CAMERA_POSITION = [0, 0.56, -0.9];
+const ENGINE_CAMERA_ROTATION = [0.5, 3.14, 0];
+
 function App() {
   const cameraRef = useRef<THREE.PerspectiveCamera>(null);
   const koenigseggRef = useRef<HTMLHeadingElement>(null);
   const gemeraRef = useRef<HTMLHeadingElement>(null);
+  const sceneRef = useRef<THREE.Group>(null);
+  const upcomingCardRef = useRef<HTMLDivElement>(null);
+  const [registerScrollTrigger, setRegisterScrollTrigger] = useState(false);
+  // const { positionX, positionY, positionZ, rotationX, rotationY, rotationZ } =
+  //   useControls({
+  //     positionX: {
+  //       value: -0.8,
+  //       min: -100,
+  //       max: 100,
+  //       step: 0.002,
+  //     },
+  //     positionY: {
+  //       value: 0.78,
+  //       min: -100,
+  //       max: 100,
+  //       step: 0.002,
+  //     },
+  //     positionZ: {
+  //       value: -1.8,
+  //       min: -100,
+  //       max: 100,
+  //       step: 0.002,
+  //     },
+  //     rotationX: {
+  //       value: 0.3,
+  //       min: -100,
+  //       max: 100,
+  //       step: 0.002,
+  //     },
+
+  //     rotationY: {
+  //       value: 3.6,
+  //       min: -100,
+  //       max: 100,
+  //       step: 0.002,
+  //     },
+  //     rotationZ: {
+  //       value: 0.13,
+  //       min: -100,
+  //       max: 100,
+  //       step: 0.002,
+  //     },
+  //   });
 
   useGSAP(() => {
-    if (!koenigseggRef.current) return;
-    if (!gemeraRef.current) return;
+    if (!registerScrollTrigger || !sceneRef.current) return;
+    console.log(sceneRef.current);
+    gsap.to(sceneRef.current?.rotation, {
+      y: Math.PI / 2,
+      scrollTrigger: {
+        trigger: upcomingCardRef.current,
+        start: "top 90%",
+        end: "top 50%",
+        scrub: true,
+        markers: true,
+      },
+    });
+  }, [registerScrollTrigger]);
+
+  useGSAP(() => {
+    if (!koenigseggRef.current || !gemeraRef.current) {
+      return;
+    }
+    console.log(sceneRef.current);
+    //Koenigsegg text animation
     gsap.from(koenigseggRef.current.children, {
       opacity: 0,
       duration: 0.4,
@@ -30,6 +100,7 @@ function App() {
       ease: "power4.in",
       stagger: 0.05,
     });
+    //Gemera text animation
     gsap.from(gemeraRef.current.children, {
       opacity: 0,
       duration: 0.6,
@@ -42,8 +113,8 @@ function App() {
 
   return (
     <Suspense fallback={<Loader />}>
-      <div className="w-screen h-screen ">
-        <section>
+      <div className="h-screen">
+        <div>
           <h1
             className="text-white text-8xl font-['Koenigsegg'] font-bold italic fixed left-[10%] top-[3%]"
             ref={koenigseggRef}
@@ -56,28 +127,54 @@ function App() {
           >
             {convertToSpan("Gemera")}
           </h1>
-        </section>
-        <section className="fixed -z-10 h-screen w-screen inset-0">
-          <Canvas className="w-screen h-screen" shadows>
+        </div>
+        <section className="w-screen fixed -z-10 inset-0 h-screen">
+          <Canvas className="" shadows>
             {/* <HeroText /> */}
             <color attach="background" args={["#0d0d0d"]} />
             <PerspectiveCamera
               ref={cameraRef}
               makeDefault
+              position={[
+                DEFAULT_CAMERA_POSITION[0],
+                DEFAULT_CAMERA_POSITION[1],
+                DEFAULT_CAMERA_POSITION[2],
+              ]}
+              fov={50}
+              rotation={[
+                DEFAULT_CAMERA_ROTATION[0],
+                DEFAULT_CAMERA_ROTATION[1],
+                DEFAULT_CAMERA_ROTATION[2],
+              ]}
+            />
+            {/* <PerspectiveCamera
+              ref={cameraRef}
+              makeDefault
               position={[-0.8, 0.78, -1.8]}
               fov={50}
               rotation={[0.3, 3.6, 0.13]}
-            />
+              /> */}
 
             {/* <OrbitControls
-            enableZoom={true}
-            enablePan={true}
-            enableRotate={true}
-            maxPolarAngle={Math.PI / 2}
+              enableZoom={true}
+              enablePan={true}
+              enableRotate={true}
+              maxPolarAngle={Math.PI / 2}
             /> */}
-            <Scene position={[0, 0, 0]} cameraRef={cameraRef} />
+
+            <Scene
+              position={[0, 0, 0]}
+              cameraRef={cameraRef}
+              ref={sceneRef}
+              setRegisterScrollTrigger={setRegisterScrollTrigger}
+            />
           </Canvas>
         </section>
+
+        <section
+          className="relative top-[150%] h-[50%] z-0 bg-amber-300"
+          ref={upcomingCardRef}
+        ></section>
       </div>
     </Suspense>
   );
--- src/Components/Scene.tsx
+++ src/Components/Scene.tsx
@@ -1,4 +1,4 @@
-import { forwardRef, useRef } from "react";
+import { forwardRef, useEffect, useRef } from "react";
 import Koenigsegg from "./Koenigsegg";
 import { useFrame } from "@react-three/fiber";
 import * as THREE from "three";
@@ -9,20 +9,30 @@ import { gsap } from "gsap";
 type SceneProps = {
   cameraRef: React.RefObject<THREE.PerspectiveCamera | null>;
   position?: [number, number, number];
+  setRegisterScrollTrigger: React.Dispatch<React.SetStateAction<boolean>>;
 } & React.ComponentPropsWithRef<"group">;
 
 const Scene = forwardRef<THREE.Group, SceneProps>(
   ({ cameraRef, ...props }, ref) => {
     const directionLightRef = useRef<THREE.DirectionalLight>(null);
+    const { setRegisterScrollTrigger } = props;
+
+    useEffect(() => {
+      if (!ref?.current) return;
+      // console.log(ref?.current);
+      setRegisterScrollTrigger(true);
+    }, []);
 
     useGSAP(() => {
       if (!cameraRef.current) return;
       gsap.from(cameraRef.current!.position, {
-        x: 10, // starting x position
-        y: 5, // starting y position
-        z: 10, // starting z position
-        // duration: 2.5,
-        duration: 7,
+        // x: 10,
+        // y: 5,
+        // z: 10,
+        y: -3,
+        z: 0,
+        x: 0,
+        duration: 4,
         ease: "power2.out",
       });
     }, []);
